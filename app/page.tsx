438c5fd1581d1475d4a194d00ce30ac0dc3e464d
alae-herrak <alaeherrak@gmail.com>
2023-11-09 19:38:07 +0100

    fix dark mode text color

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default async function Home() {
   return (
     <main className="flex h-screen flex-col items-center justify-center gap-10 bg-gradient-to-bl from-sky-950 via-zinc-950 to-slate-950">
       <div>
-        <h1 className="text-4xl font-semibold">Task Manager</h1>
+        <h1 className="text-4xl font-semibold dark:text-white">Task Manager</h1>
         <p className="text-center font-serif text-gray-300">By Alae Herrak</p>
       </div>
       <Link href="/dashboard">
